513c311965524ae9a7256cb58a33a44d68605ba9
JamesOdera <nyangoma.odera@gmail.com>
2020-02-28 11:50:58 +0300

    create a template inheritance for our project

--- image/views.py
+++ image/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
 
@@ -8,6 +8,7 @@ def gallery(request):
 
 def image_of_day(request):
     date = dt.date.today()
+    return render(request, 'images/today-image.html', {"date": date,})
 
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
     day = convert_dates(date)
@@ -31,16 +32,18 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-def past_days_image(request,past_date):
-    # Converts data from the string Url
-    date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+def past_days_image(request, past_date):
 
-    day = convert_dates(date)
-    html = f'''
-        <html>
-            <body>
-                <h1>Image for {day} {date.day}-{date.month}-{date.year}</h1>
-            </body>
-        </html>
-            '''
-    return HttpResponse(html)
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(news_of_day)
+
+    return render(request, 'images/past-image.html', {"date": date})
